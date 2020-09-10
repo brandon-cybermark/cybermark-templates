c8b16e479ba6c3cda1278b95cb2c0372d2ec453e
Dallas Washburn <dallas@cybermark.com>
2020-09-10 09:33:17 -0700

    fixed last gallery slide

---
+++ Galleries/Gallery4.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function () {
+    $(".regular").slick({
+        dots: true,
+        arrows:true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3
+    });
+});
